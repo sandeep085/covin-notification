6fe3315a27316cfe44662d051371b9d484ef835b
sandeep085 <085sandeep@gmail.com>
2021-05-08 15:57:56 +0530

    script updated

--- covin.py
+++ covin.py
@@ -12,21 +12,22 @@ receiverEmail = input("Type receiver email and press enter: ")
 bcc = input("Type Bcc email and press enter: ")
 pinCode = input("Type pincode of the place you want to schedule for and press enter: ")
 maxDaysThresholdDays = input("Type(must be numeric) till how many days starting from now you want to crawl for and press enter: ")
-waitTime = input("Type wait time for each execution in seconds(numeric) and press enter: ")
+waitTime = input("Type wait time for each execution in seconds(numeric and must be greater than 1 min i.e 60 seconds) and press enter: ")
+
+waitSeconds = int(waitTime)
+print(waitSeconds)
+if waitSeconds < 60:
+        print("Since waitTime is less then 60 seconds , resetting it to 60 secs ")
+        waitSeconds = 60
+
 
 def sendMail(mailContent):
         port = 465  # For SSL
-        #password = input("Type your password and press enter: ")
-        #password = 'Darsh@110616'
-        #senderEmail = "sn.samual@gmail.com"
-        #receiverEmail = "meenuc95@gmail.com"
-        #bcc = "085sandeep@gmail.com"
         message = MIMEMultipart()
         message["From"] = senderEmail
         message["To"] = receiverEmail
         message["Subject"] = "CoWin Availablity"
         message["Bcc"] = bcc  # Recommended for mass emails
-
         # Add body to email
         message.attach(MIMEText(mailContent, "plain"))
         context = ssl.create_default_context()
@@ -42,22 +43,17 @@ def sendMail(mailContent):
                         server.quit()
 
 
-def do_something(sc):
+def execute(sc):
         print("Doing stuff...")
         now = datetime.datetime.now()
         # Currently I just want to run this logic b/w 7 AM to 7 P.M, Modify accordingly
         if (now.hour <7 or now.hour >18):
-                print(now.hour)
+                print("Not a suitable execution time, Please try this script b/w & A.M to & P.M else modify the limits")
                 return
-
-        #URL = "https://www.cowin.gov.in/api/v2/appointment/sessions/public/calendarByPin?pincode=121001&date=08-05-2021"
-        #r = requests.get(URL)
-        #data = r.json()
 
         currDate = datetime.datetime.today().strftime('%d-%m-%Y')
         maxDaysThreshold = datetime.timedelta(int(maxDaysThresholdDays))
         maxDate = datetime.datetime.today() + maxDaysThreshold
-        #maxDate = datetime.datetime.strptime(maxDate, '%d-%m-%Y')
         delta = datetime.timedelta(days=1)
         startDate = datetime.datetime.strptime(currDate, '%d-%m-%Y')
         while startDate <= maxDate:
@@ -66,11 +62,11 @@ def do_something(sc):
                 print(URL)
                 startDate += delta
                 # For Testing Only
-                f = open('test.json', )
-                data = json.load(f)
+               # f = open('test.json', )
+               # data = json.load(f)
                 #Real Hit
-               # r = requests.get(URL)
-               # data = r.json()
+                r = requests.get(URL)
+                data = r.json()
                 print("Response DATA : " + str(data))
                 messageContent = ""
                 for key in data:
@@ -83,15 +79,13 @@ def do_something(sc):
                                         if ss["available_capacity"] > 0:
                                                 messageContent =  messageContent+"\n"+"Vaccine available for date : {} at Address : {}  ,Available Quota  : {}".format(
                                                                 ss["date"], k2["address"], ss["available_capacity"])
-                                                sendMail(messageContent)
-                                                # print("Vaccine Available")
-                                        else:
-                                                #my_function("Vaccine Not Available")
-                                                print("Vaccine Not Available")
+
                 if messageContent :
                         sendMail(messageContent)
-                s.enter(int(waitTime), 1, do_something, (sc,))
+                else :
+                        print("Vaccine Not Available For Date : "+ dateString)
+                s.enter(waitSeconds, 1, execute, (sc,))
 
 
-s.enter(int(waitTime), 1, do_something, (s,))
+s.enter(waitSeconds, 1, execute, (s,))
 s.run()
